8a3e6bbfade126122fe94a8d9058a0ae5f47fed3
karim-w <karim.wael@gmail.com>
2022-08-06 18:50:25 +0400

    init

---
+++ httpclient.go
@@ -0,0 +1,170 @@
+package stdlib
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type Client interface {
+	Get(ctx context.Context, url string, opt *ClientOptions, dest interface{}) (int, error)
+	Put(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error)
+	Patch(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error)
+	Post(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error)
+	Del(ctx context.Context, url string, opt *ClientOptions, dest interface{}) (int, error)
+	doRequest(ctx context.Context, opt *ClientOptions, body interface{}, dest interface{}) (int, error)
+}
+
+type ClientOptions struct {
+	Authorization string             `json:"authorization"`
+	ContentType   string             `json:"content_type"`
+	Headers       *map[string]string `json:"headers"`
+	Timeout       *time.Time         `json:"timeout"`
+	RequestType   string             `json:"request_type"`
+	url           string
+	method        string
+}
+
+type httpCLientImpl struct {
+	logger *zap.Logger
+	c      http.Client
+}
+
+func ClientProvider(l *zap.Logger) Client {
+	if l == nil {
+		if log, err := zap.NewProduction(); err != nil {
+			return &httpCLientImpl{
+				logger: log,
+				c:      http.Client{},
+			}
+		} else {
+			panic("Could not create the http client due to error generating zap logger: " + err.Error())
+		}
+	} else {
+		return &httpCLientImpl{
+			logger: l,
+			c:      http.Client{},
+		}
+	}
+}
+
+func (h *httpCLientImpl) Get(ctx context.Context, url string, opt *ClientOptions, dest interface{}) (int, error) {
+	opt.method = "GET"
+	return h.doRequest(ctx, opt, nil, dest)
+}
+
+func (h *httpCLientImpl) Put(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error) {
+	opt.method = "PUT"
+	return 0, nil
+}
+
+func (h *httpCLientImpl) Patch(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error) {
+	opt.method = "PATCH"
+	return 0, nil
+}
+
+func (h *httpCLientImpl) Post(ctx context.Context, url string, opt *ClientOptions, body interface{}, dest interface{}) (int, error) {
+	opt.method = "POST"
+	return 0, nil
+}
+
+func (h *httpCLientImpl) Del(ctx context.Context, url string, opt *ClientOptions, dest interface{}) (int, error) {
+	opt.method = "DELETE"
+	return 0, nil
+}
+
+func (h *httpCLientImpl) doRequest(ctx context.Context, opt *ClientOptions, body interface{}, dest interface{}) (int, error) {
+	logger := extractLoggerFromContext(ctx, h.logger)
+	if req, err := http.NewRequest(opt.method, opt.url, nil); err != nil {
+		return 0, err
+	} else {
+		if contentType, reqBody, err := h.formulatePayload(body, opt.RequestType); err != nil {
+			return 0, err
+		} else {
+			hd := opt.Headers
+			for k, v := range *hd {
+				req.Header.Set(k, v)
+			}
+			req.Header.Add("Accept", "application/json")
+			req.Header.Add("Content-Type", contentType)
+			if body != nil {
+				req.Body = reqBody
+			}
+			now := time.Now()
+			defer func() {
+				latency := time.Since(now).Milliseconds()
+				logger.Sugar().Infof("[%s:%s] Latency:%d", opt.method, opt.url, latency)
+			}()
+			if resp, err := h.c.Do(req); err != nil {
+				return 0, err
+			} else {
+				if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+					if resp.Body != nil && resp.ContentLength != 0 {
+						if err := json.NewDecoder(resp.Body).Decode(dest); err != nil {
+							return resp.StatusCode, fmt.Errorf("error decoding response: %v", err)
+						}
+					}
+					return resp.StatusCode, nil
+				} else {
+					return resp.StatusCode, fmt.Errorf("got non 200 code (%d) calling %s", resp.StatusCode, opt.url)
+				}
+			}
+		}
+	}
+}
+
+func (h *httpCLientImpl) formulatePayload(body interface{}, rType string) (string, io.ReadCloser, error) {
+	switch rType {
+	case "json":
+		if strBody, err := json.Marshal(body); err != nil {
+			return "", nil, err
+		} else {
+			return "application/json", ioutil.NopCloser(bytes.NewBuffer(strBody)), nil
+		}
+	case "www-form-urlencoded":
+		if kv, ok := body.(map[string]interface{}); ok {
+			strBody := ""
+			for k, v := range kv {
+				strBody += k + "=" + fmt.Sprintf("%v", v) + "&"
+			}
+			//remove last &
+			strBody = strBody[:len(strBody)-1]
+			return "application/x-www-form-urlencoded", ioutil.NopCloser(bytes.NewBuffer([]byte(strBody))), nil
+		} else {
+			return "", nil, fmt.Errorf("invalid body type for www-form-urlencoded")
+		}
+	case "form-data":
+		if kv, ok := body.(map[string]interface{}); !ok {
+			return "", nil, fmt.Errorf("body must be a map[string]interface{} ideally map[string]string but i compromised")
+		} else {
+			payload := &bytes.Buffer{}
+			writer := multipart.NewWriter(payload)
+			_ = writer.WriteField("r", "r")
+			_ = writer.WriteField("s", "s")
+			for k, v := range kv {
+				if str, ok := v.(string); ok {
+					_ = writer.WriteField(string(k), str)
+				} else {
+					_ = writer.WriteField(string(k), fmt.Sprintf("%v", v))
+				}
+			}
+			if err := writer.Close(); err != nil {
+				return "", nil, err
+			} else {
+				return "multipart/form-data", ioutil.NopCloser(payload), nil
+			}
+		}
+	case "graphql":
+		return "", nil, fmt.Errorf("graphql is not supported yet")
+	default:
+		return "", nil, fmt.Errorf("unknown request type")
+	}
+}
