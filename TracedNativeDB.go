a63a7975798a077821d00bf9654eb4f9e4106b26
Karim <karim.wael@gmail.com>
2022-09-14 08:48:50 +0400

    traced db

--- TracedNativeDB.go
+++ TracedNativeDB.go
@@ -1,37 +1,44 @@
 package stdlib
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"sync"
 	"time"
 
+	tracer "github.com/BetaLixT/appInsightsTrace"
 	_ "github.com/lib/pq"
+	"go.uber.org/zap"
 )
 
 type NativeDatabase interface {
 	Begin() (*sql.Tx, error)
-	BeginTx(ctx Context, opts *sql.TxOptions) (*sql.Tx, error)
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 	Close() error
-	Conn(ctx Context) (*sql.Conn, error)
+	Conn(ctx context.Context) (*sql.Conn, error)
 	Exec(query string, args ...any) (sql.Result, error)
-	ExecContext(ctx Context, query string, args ...any) (sql.Result, error)
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	Ping() error
-	PingContext(ctx Context) error
+	PingContext(ctx context.Context) error
 	Prepare(query string) (*sql.Stmt, error)
-	PrepareContext(ctx Context, query string) (*sql.Stmt, error)
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
 	Query(query string, args ...any) (*sql.Rows, error)
-	QueryContext(ctx Context, query string, args ...any) (*sql.Rows, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	QueryRow(query string, args ...any) *sql.Row
-	QueryRowContext(ctx Context, query string, args ...any) *sql.Row
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
 	SetConnMaxIdleTime(d time.Duration)
 	SetConnMaxLifetime(d time.Duration)
 	Stats() sql.DBStats
 }
 
 type dbImpl struct {
-	logger   *logger
+	logger   *zap.Logger
 	db       *sql.DB
 	pingLock sync.Mutex
+	t        *tracer.AppInsightsCore
+	driver   string
+	name     string
 }
 
 func NativeDatabaseProvider(Driver string, DSN string) NativeDatabase {
@@ -43,9 +50,10 @@ func NativeDatabaseProvider(Driver string, DSN string) NativeDatabase {
 		} else {
 			l.Info("[DATABASE]\tSucessfuly Connected to postgres database")
 			ndb := &dbImpl{
-				logger:   getLoggerInstance(),
+				logger:   getLoggerInstance().logger.Desugar(),
 				db:       db,
 				pingLock: sync.Mutex{},
+				driver:   Driver,
 			}
 			ndb.Ping()
 			return ndb
@@ -55,42 +63,105 @@ func NativeDatabaseProvider(Driver string, DSN string) NativeDatabase {
 	}
 }
 
+func TracedNativeDBWrapper(
+	Driver string,
+	DSN string,
+	t *tracer.AppInsightsCore,
+	name string,
+) NativeDatabase {
+	l := getLoggerInstance()
+	switch Driver {
+	case "postgres":
+		if db, err := sql.Open("postgres", DSN); err != nil {
+			panic(err)
+		} else {
+			l.Info("[DATABASE]\tSucessfuly Connected to postgres database")
+			ndb := &dbImpl{
+				logger:   getLoggerInstance().logger.Desugar(),
+				db:       db,
+				pingLock: sync.Mutex{},
+				t:        t,
+				driver:   Driver,
+				name:     name,
+			}
+			ndb.Ping()
+			return ndb
+		}
+	default:
+		panic("Unsupported driver")
+	}
+}
+
+func (d *dbImpl) WithLogger(l *zap.Logger) *dbImpl {
+	d.logger = l
+	return d
+}
+
 func (d *dbImpl) Begin() (*sql.Tx, error) {
 	return d.db.Begin()
 }
 
-func (d *dbImpl) BeginTx(ctx Context, opts *sql.TxOptions) (*sql.Tx, error) {
-	return d.db.BeginTx(ctx.Context, opts)
+func (d *dbImpl) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
+	return d.db.BeginTx(ctx, opts)
 }
 
 func (d *dbImpl) Close() error {
 	return d.db.Close()
 }
 
-func (d *dbImpl) Conn(ctx Context) (*sql.Conn, error) {
-	return d.db.Conn(ctx.Context)
+func (d *dbImpl) Conn(ctx context.Context) (*sql.Conn, error) {
+	return d.db.Conn(ctx)
 }
 
 func (d *dbImpl) Exec(query string, args ...any) (sql.Result, error) {
 	now := time.Now()
-	if res, err := d.db.Exec(query, args...); err != nil {
-		d.logger.Errorf("[DATABASE]\tError executing query: %s with args: %v with error : %w ", query, args, err)
-		return res, err
+	res, err := d.db.Exec(query, args...)
+	elapsed := float64(time.Since(now).Microseconds()) / 1000.0
+	if err != nil {
+		d.logger.Error("[DATABASE]  Error executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Error(err))
 	} else {
-		d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
-		return res, err
+		d.logger.Info("[DATABASE]  Executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Float64("elapsed(ms)", elapsed))
 	}
+	return res, err
 }
 
-func (d *dbImpl) ExecContext(ctx Context, query string, args ...any) (sql.Result, error) {
+func (d *dbImpl) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
 	now := time.Now()
-	if res, err := d.db.ExecContext(ctx.Context, query, args...); err != nil {
-		d.logger.Errorf("[DATABASE]\tError executing query: %s with args: %v with error : %w ", query, args, err)
-		return res, err
+	res, err := d.db.ExecContext(ctx, query, args...)
+	after := time.Now()
+	elapsed := float64(after.Sub(now).Microseconds()) / 1000.0
+	if err != nil {
+		d.logger.Error("[DATABASE]  Error executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Error(err))
+		d.t.TraceException(ctx, err, 0, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+			"error": err.Error(),
+		})
+		d.t.TraceDependency(ctx, "", d.driver, d.name, "EXEC"+query, false, now, after, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+			"error": err.Error(),
+		})
 	} else {
-		d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
-		return res, err
+		d.logger.Info("[DATABASE]  Executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Float64("elapsed(ms)", elapsed))
+		d.t.TraceDependency(ctx, "", d.driver, d.name, "EXEC"+query, true, now, after, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+		})
 	}
+	return res, err
 }
 
 func (d *dbImpl) Ping() error {
@@ -104,51 +175,93 @@ func (d *dbImpl) Ping() error {
 	return d.db.Ping()
 }
 
-func (d *dbImpl) PingContext(ctx Context) error {
-	return d.db.PingContext(ctx.Context)
+func (d *dbImpl) PingContext(ctx context.Context) error {
+	return d.db.PingContext(ctx)
 }
 
 func (d *dbImpl) Prepare(query string) (*sql.Stmt, error) {
 	return d.db.Prepare(query)
 }
 
-func (d *dbImpl) PrepareContext(ctx Context, query string) (*sql.Stmt, error) {
-	return d.db.PrepareContext(ctx.Context, query)
+func (d *dbImpl) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	return d.db.PrepareContext(ctx, query)
 }
 
 func (d *dbImpl) Query(query string, args ...any) (*sql.Rows, error) {
 	now := time.Now()
-	if res, err := d.db.Query(query, args...); err != nil {
-		d.logger.Errorf("[DATABASE]\tError executing query: %s with args: %v with error : %w ", query, args, err)
-		return res, err
+	res, err := d.db.Query(query, args...)
+	elapsed := float64(time.Since(now).Microseconds()) / 1000.0
+	if err != nil {
+		d.logger.Error("[DATABASE]  Error executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Error(err))
 	} else {
-		d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
-		return res, err
+		d.logger.Info("[DATABASE]  Executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Float64("elapsed(ms)", elapsed))
 	}
+	return res, err
 }
 
-func (d *dbImpl) QueryContext(ctx Context, query string, args ...any) (*sql.Rows, error) {
+func (d *dbImpl) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
 	now := time.Now()
-	if res, err := d.db.QueryContext(ctx.Context, query, args...); err != nil {
-		d.logger.Errorf("[DATABASE]\tError executing query: %s with args: %v with error : %w ", query, args, err)
-		return nil, err
+	res, err := d.db.QueryContext(ctx, query, args...)
+	after := time.Now()
+	elapsed := float64(after.Sub(now).Microseconds()) / 1000.0
+	if err != nil {
+		d.logger.Error("[DATABASE]  Error executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Error(err))
+		d.t.TraceException(ctx, err, 0, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+			"error": err.Error(),
+		})
+		d.t.TraceDependency(ctx, "", d.driver, d.name, "EXEC"+query, false, now, after, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+			"error": err.Error(),
+		})
 	} else {
-		d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
-		return res, err
+		d.logger.Info("[DATABASE]  Executing query",
+			zap.String("query", query),
+			zap.Any("args", args),
+			zap.Float64("elapsed(ms)", elapsed))
+		d.t.TraceDependency(ctx, "", d.driver, d.name, "EXEC"+query, true, now, after, map[string]string{
+			"query": query,
+			"args":  fmt.Sprintf("%v", args),
+		})
 	}
+	return res, err
 }
 
 func (d *dbImpl) QueryRow(query string, args ...any) *sql.Row {
 	now := time.Now()
 	r := d.db.QueryRow(query, args...)
-	d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
+	elapsed := float64(time.Since(now).Microseconds()) / 1000.0
+	d.logger.Info("[DATABASE]  Executing query",
+		zap.String("query", query),
+		zap.Any("args", args),
+		zap.Float64("elapsed(ms)", elapsed))
 	return r
 }
 
-func (d *dbImpl) QueryRowContext(ctx Context, query string, args ...any) *sql.Row {
+func (d *dbImpl) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
 	now := time.Now()
-	r := d.db.QueryRowContext(ctx.Context, query, args...)
-	d.logger.Infof("[DATABASE]\t[%0.2f ms]\tExecuting query: %s with args: %v", float64(time.Since(now).Microseconds())/1000.0, query, args)
+	r := d.db.QueryRowContext(ctx, query, args...)
+	after := time.Now()
+	elapsed := float64(after.Sub(now).Microseconds()) / 1000.0
+	d.logger.Info("[DATABASE]  Executing query",
+		zap.String("query", query),
+		zap.Any("args", args),
+		zap.Float64("elapsed(ms)", elapsed))
+	d.t.TraceDependency(ctx, "", d.driver, d.name, "EXEC"+query, true, now, after, map[string]string{
+		"query": query,
+		"args":  fmt.Sprintf("%v", args),
+	})
 	return r
 }
 
--- TracedNativeDB_test.go
+++ TracedNativeDB_test.go
@@ -1,9 +1,6 @@
 package stdlib
 
 import (
-	"fmt"
-	"testing"
-
 	_ "github.com/lib/pq"
 )
 
@@ -15,25 +12,25 @@ const (
 	dbname   = "temp"
 )
 
-func TestNativeDbCon(t *testing.T) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	d := NativeDatabaseProvider("postgres", psqlInfo)
-	res, err := d.ExecContext(NewContext(), "DROP TABLE IF EXISTS tt")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res)
-	res, err = d.ExecContext(NewContext(), "INSERT INTO vibes VALUES (3,'hello',1,'ABC')")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res)
-	res, err = d.ExecContext(NewContext(), "INSERT INTO vibes VALUES (4,'hello',2,'ABC')")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res)
+// func TestNativeDbCon(t *testing.T) {
+// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
+// 	d := NativeDatabaseProvider("postgres", psqlInfo)
+// 	res, err := d.ExecContext(NewContext(), "DROP TABLE IF EXISTS tt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(res)
+// 	res, err = d.ExecContext(NewContext(), "INSERT INTO vibes VALUES (3,'hello',1,'ABC')")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(res)
+// 	res, err = d.ExecContext(NewContext(), "INSERT INTO vibes VALUES (4,'hello',2,'ABC')")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(res)
 
-}
+// }
