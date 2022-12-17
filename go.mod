6640b8d3f5eaaa09984d29e2d47661627e1d2c4a
Karim-W <karim.wael@gmail.com>
2022-12-17 20:04:56 +0400

    [FEATURE] added InvokeMethod and testify for tests

--- go.mod
+++ go.mod
@@ -5,13 +5,17 @@ go 1.19
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/stretchr/testify v1.8.0
 	go.uber.org/zap v1.23.0
 )
 
 require (
 	code.cloudfoundry.org/clock v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/microsoft/ApplicationInsights-Go v0.4.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- http_test.go
+++ http_test.go
@@ -1,99 +1,14 @@
 package stdlib
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
-	"go.uber.org/zap"
+	"github.com/stretchr/testify/assert"
 )
 
-type UserRoleRequest struct {
-	Args    Args        `json:"args"`
-	Data    string      `json:"data"`
-	Files   Args        `json:"files"`
-	Form    Args        `json:"form"`
-	Headers Headers     `json:"headers"`
-	JSON    interface{} `json:"json"`
-	Origin  string      `json:"origin"`
-	URL     string      `json:"url"`
-}
-
-type Args struct {
-}
-
-type Headers struct {
-	Accept         string `json:"Accept"`
-	AcceptEncoding string `json:"Accept-Encoding"`
-	AcceptLanguage string `json:"Accept-Language"`
-	ContentLength  string `json:"Content-Length"`
-	ContentType    string `json:"Content-Type"`
-	Host           string `json:"Host"`
-	Origin         string `json:"Origin"`
-	Referer        string `json:"Referer"`
-	UserAgent      string `json:"User-Agent"`
-	XAmznTraceID   string `json:"X-Amzn-Trace-Id"`
-}
-
-func TestGetRequst(t *testing.T) {
-	c, _ := ClientProvider()
-	res := map[string]interface{}{}
-	ctx := context.TODO()
-	stat, err := c.Get(ctx, "http://localhost:8081/health-check", nil, &res)
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(res)
-	fmt.Println(stat)
-}
-
-func TestCreateClient(t *testing.T) {
-	c, _ := ClientProvider()
-	res := UserRoleRequest{}
-	body := map[string]interface{}{
-		"name": "test",
-	}
-	ctx := context.TODO()
-	stat, err := c.Post(ctx, "https://httpbin.org/post", nil, body, &res)
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(res)
-	fmt.Println(stat)
-}
-func TestInvalidCreateTracedClient(t *testing.T) {
-	l, err := zap.NewProduction()
-	if err != nil {
-		t.Error(err)
-	}
-	c := TracedClientProvider(nil, l)
-	ctx := context.TODO()
-	body := map[string]interface{}{
-		"name": "test",
-	}
-	var res interface{}
-	stat, err := c.Post(ctx, "https://AAAAhttpbin.org/post", nil, body, &res)
-	if err == nil {
-		t.Error("Expected error")
-	}
-	fmt.Println(res)
-	fmt.Println(stat)
-}
-func TestValidCreateTracedClient(t *testing.T) {
-	l, err := zap.NewProduction()
-	if err != nil {
-		t.Error(err)
-	}
-	c := TracedClientProvider(nil, l)
-	ctx := context.TODO()
-	body := map[string]interface{}{
-		"name": "test",
-	}
-	var res interface{}
-	stat, err := c.Post(ctx, "https://httpbin.org/post", nil, body, &res)
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(res)
-	fmt.Println(stat)
+func TestTestPostionalArgs(t *testing.T) {
+	str := "https://example.com/{server}/test/{test_name}"
+	args := []string{"server1", "test1"}
+	result := EmbedNamedPositionArgs(str, args...)
+	assert.Equal(t, "https://example.com/server1/test/test1", result)
 }
