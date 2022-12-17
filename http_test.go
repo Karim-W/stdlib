972f197a0419698833519d5bb467e0888c23335d
Karim-W <karim.wael@gmail.com>
2022-12-17 21:52:57 +0400

    [FIX] Invoke Method fix

--- http_test.go
+++ http_test.go
@@ -1,6 +1,7 @@
 package stdlib
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,13 @@ func TestTestPostionalArgs(t *testing.T) {
 	args := []string{"server1", "test1"}
 	result := EmbedNamedPositionArgs(str, args...)
 	assert.Equal(t, "https://example.com/server1/test/test1", result)
+}
+
+func TestInvokeGetRequest(t *testing.T) {
+	url := "https://httpbin.org/get"
+	cl, err := ClientProvider()
+	assert.Nil(t, err)
+	code, err := cl.Invoke(context.TODO(), "GET", url, nil, nil, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, code)
 }
